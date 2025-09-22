453f390e0c2d578b2562ed3b837d33217e0d9097
Stephen Fraga <stephen@stephenfraga.com>
2025-09-21 19:41:20 -0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        <title>Precruits: Jump the Line for Recruiting</title>
+    </head>
+    <body>
+        <h1>Precruits: Jump the Line for Recruiting!!</h1>
+    </body>
+</html>
